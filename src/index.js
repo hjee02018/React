ab7e1fac1a554289486b8ff211d317b064208d77
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2023-11-23 11:54:23 +0900

    [feat] : Add End Page
    THINGS DONE
    * Created component named Result to hold title/image/explanation - width and height of image and its source can also be altered
    타이틀/이미지/설명 구조를 가지는 컴포넌트 생성 - 이미지의 가로 세로 길이와 소스 링크 조정 가능
    
    * Words inside SVG Image buttons are highlighted when hovered
    SVG 이미지 버튼에 마우스를 올리면 안의 흰 글씨가 노란색으로 바뀌는 기능 추가
    
    * Explanation is added with innerHTML and can sense <br>
    컴포넌트 Result의 설명란(explain) 내용은 <br>을 인식하도록 innerHTML으로 추가되게 함
    
    THINGS TO DO
    * [fix] : Small margin exists after .endPageHeader and .endPageFooter
    버그 - 헤더와 푸터 다음에 마진이 생긴다.
    * [feat] : Need to add "Korea National School of Tricks" Logo at the bottom but want to check with other developers on how to do it
    한국술법학교 로고를 밑에 추가해야하지만 추가하는 방식을 다른 개발자들과 논의 후 하기로 함

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Routes, Route, Link, BrowserRouter, createBrowserRouter, RouterProvider,} from 'react-router-dom';
+import EndPage from './endPage/endpage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "end",
+    element: <EndPage/>
+  },  
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
