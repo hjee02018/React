f0ed365f1edfcaced82b415ff573838ac1e2ae0c
hjee02018@naver.com <hjee02018@naver.com>
2023-11-29 14:27:19 +0900

    hjyang : branch creation

--- src/main.js
+++ src/main.js
@@ -1,27 +1,39 @@
-// src/main.js
 import React, { useState } from "react";
 import InfoComponent from "./info";
+import Room2 from "./room2/Room2";
+import Room1 from "./room1/Room1";
+
 import img from "./res/main_bg.png";
 
 function MainComponent() {
   const [showInfo, setShowInfo] = useState(false);
+  const [showRoom2, setShowRoom2] = useState(false);
 
   const handleInfoClick = () => {
     setShowInfo(true);
   };
 
+  const handleStartClick = () => {
+    setShowInfo(false);
+    setShowRoom2(true);
+  };
+
   const imgStyle3 = {
-    width: "100%", // 이미지의 너비를 100%로 설정
-    height: "100vh", // 이미지의 높이를 100%로 설정
+    width: "100%",
+    height: "100vh",
     top: 0,
     left: 0,
     position: "fixed",
+    backgroundRepeat: "repeat-y", // 수직으로 반복
+    backgroundSize: "100% auto", // 크기 설정
   };
 
   return (
     <div>
       {showInfo ? (
         <InfoComponent />
+      ) : showRoom2 ? (
+        <Room1 />
       ) : (
         <>
           <img src={img} style={imgStyle3} alt="background"></img>
@@ -37,6 +49,7 @@ function MainComponent() {
             src="./res/start_cta.svg"
             alt="Start CTA"
             className="cta-txt-image2"
+            onClick={handleStartClick}
           />
           <img src="./res/logo_w.svg" alt="logo CTA" className="logo-image" />
         </>
